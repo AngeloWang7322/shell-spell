51caf8f1621dbdeff76a7dd80999a7348e4faa8a
Angelo-Wang <angelo.fu.wa@gmail.com>
2025-10-28 17:10:45 +0100

    File Structure genauer organisiert

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
-<?php 
-    echo "<p>Hello World!</p>";
-?>asdsa
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+
+$routes = [
+    '/' => 'pages/game.php',
+    '/login' => 'pages/login.php',
+    '/profile' => 'pages/profile.php',
+    '/notfound' => 'pages/notfound.php'
+];
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$path = trim($path, '/');
+
+if (isset($routes[$path])) 
+{
+    require __DIR__ . '/' . $routes[$path];
+}
+else
+{
+    require __DIR__ . '/' . $routes['/notfound'];
+}
